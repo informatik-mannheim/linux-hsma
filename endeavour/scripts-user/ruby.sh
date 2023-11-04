312db16ab487826d9bdf1d5bfef6bad4699491d7
Thomas Smits <thomsmits@gmail.com>
2023-11-04 15:18:39 +0100

    Add rbenv to shell

--- endeavour/scripts-user/ruby.sh
+++ endeavour/scripts-user/ruby.sh
@@ -5,3 +5,10 @@ git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-b
 
 # Install ruby
 rbenv install 3.2.2
+
+# Activate version
+rbenv global 3.2.2
+
+# Add rbenv to bash and fish
+echo "eval \"\$(rbenv init - bash)\"" >> ~/.bash_profile
+echo "status --is-interactive; and rbenv init - fish | source" >> ~/.config/fish/config.fish
